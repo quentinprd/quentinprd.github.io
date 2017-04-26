3afb1a39fdfcb0c9c5dae47460804ecf0463e2a9
quentinprd <quentinprodhomme@hotmail.fr>
2017-04-26 08:45:44 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,79 @@
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myObj = this.responseText;
-        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("demo").innerHTML = jsonPretty;
-    }
-};
-xmlhttp.open("GET", "http://bitcoin.mubiz.com/networkhashps", true);
-xmlhttp.send();
+$(document).ready(function() {
+	$.ajax({
+		url : "https://bitcoin.mubiz.com/info",
+		dataType : "json",
+	
+		type : "GET",
+		timeout: "5000",
+		async : true,
+	
+		success : function(data) {
+			console.log(data);
+			$('#bitcoin_block_number').append(data.blocks);
+		},
+
+		error : function(xhr, status, err) {
+			$('#bitcoin_block_number').append(err+" N/A");
+           
+		}
+	});
 });
 
-function output(inp) {
-    document.body.appendChild(document.createElement('pre')).innerHTML = inp;
-}
 
-function syntaxHighlight(json) {
-    json = json.replace(/&/g, '&').replace(//g, '>');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-            } else {
-                cls = 'string';
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-        } else if (/null/.test(match)) {
-            cls = 'null';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-    });
-}
 
-output(str);
-output(syntaxHighlight(str));
+
+$(document).ready(function() {
+	$.ajax({
+		url : "http://blockchain.info/fr/ticker?cors=true",
+		dataType : "json",
+		
+		type : "GET",
+		timeout: "5000",
+		async : true,
+
+		success : function(data) {
+			$('#bitcoin_usd_price').append(data.EUR["15m"]+' '+data.EUR.symbol);
+		},
+
+		error : function(xhr, status, err) {
+			$('#bitcoin_usd_price').append(err+" N/A");
+		}
+	});
+});
+
+
+
+
+
+$(document).ready(function() {
+	$.ajax({
+		url : "https://api.blockchain.info/stats?cors=true",
+		dataType : "json",
+		type : "GET",
+		timeout: "5000",
+		async : true,
+
+		success : function(data) {
+			$('#bitcoin_blocks').append(data.minutes_between_blocks+" min");
+			$('#bitcoin_network_hash').append(data.difficulty);
+		},
+
+		error : function(xhr, status, err) {
+			$('#bitcoin_blocks').append(err+" N/A");
+			$('#bitcoin_network_hash').append(err+" N/A");
+		}
+	});
+
+
+	
+});
+
+
+function titreHover(){
+		$('#titre').text("INFO - Blockchain");
+	}
+
+function titreNoHover(){
+
+		$('#titre').text("Bitcoin");
+	}
